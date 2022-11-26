c75b4accf3ca3b05b59a2ab288a144642402b997
Ronak Munjapara <43374534+ronakmunjapara@users.noreply.github.com>
2022-11-26 16:40:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pickle
 import streamlit as st
 import requests
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=SECRATE&language=en-US".format(movie_id)
     data = requests.get(url)
     data = data.json()
     poster_path = data['poster_path']
